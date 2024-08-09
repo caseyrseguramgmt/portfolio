abfe8e0b5cba26b4a9fd7f679101d5e975e6d547
Casey Segura <caseyrseguramgmt@gmail.com>
2024-08-09 01:41:43 -0400

    temp link for real project

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Temporary link for real project
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="http://localhost:5173/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          http://localhost:5173/
         </a>
       </header>
     </div>
