6a96e6c8980eee0b5587f0ee611765062ad4f74c
Casey Segura <caseyrseguramgmt@gmail.com>
2024-08-09 01:58:29 -0400

    temp link for real projectv4

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders link with herf to localhost', () => {
+test('renders link with href to localhost', () => {
   render(<App />);
-  const linkElement = screen.getByRole('link', { name: /learn react/i });
-    expect(linkElement).toHaveAttribute('href', 'http://localhost:5173/');
-  });
+  const linkElement = screen.getByRole('link', { name: /http:\/\/localhost:5173\//i });
+  expect(linkElement).toHaveAttribute('href', 'http://localhost:5173/');
+});
